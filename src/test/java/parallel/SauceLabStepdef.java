823784a0065cdb0a5b47d2dda3c6ee976204446b
poonams21 <poonammsingh21@gmail.com>
2025-01-31 18:57:11 +0530

    SauceLab02
    FatureFile Folder name changes

--- src/test/java/parallel/SauceLabStepdef.java
+++ src/test/java/parallel/SauceLabStepdef.java
@@ -5,11 +5,15 @@ import com.pages.SauceLabLoginPage;
 import com.qa.factory.DriverFactory;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 import java.util.List;
 import java.util.Map;
 
 public class SauceLabStepdef {
+    private static String title;
     private SauceLabLoginPage loginPage = new SauceLabLoginPage(DriverFactory.getDriver());
     private InventoryPage inventoryPage;
 
@@ -30,4 +34,15 @@ public class SauceLabStepdef {
         String title = inventoryPage.getAccountsPageTitle();
         System.out.println("Accounts Page title is: " + title);
     }
+    @When("user gets the title of the SauceLabPage")
+    public void user_gets_the_title_of_the_sauceLabpage() {
+        title = loginPage.getLoginPageTitle();
+        System.out.println("Page title is: " + title);
+    }
+
+    @Then("SauceLabPage title should be {string}")
+    public void sauceLabpage_title_should_be(String expectedTitleName) {
+        Assert.assertTrue(title.contains(expectedTitleName));
+    }
+
 }
